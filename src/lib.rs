ad2a258d3c713f2501adaa8e41b4c135036c830b
Thomas Sieverding <Thomas@bajix.com>
2022-01-06 01:04:02 -0800

    Add ClaimAllPending mode for work-resumation on restarts for single-consumer usage

--- src/lib.rs
+++ src/lib.rs
@@ -278,16 +278,20 @@ pub enum ConsumerGroupState {
   PreviouslyCreated,
 }
 
-/// [`EventReactor`] claim mode
+/// Claim modes governing work-stealing from other consumers
 pub enum ClaimMode {
-  /// Process new entries while clearing the idle-pending backlog until none remain and max_idle has elapsed
+  /// Claim all pending entries from other consumers irregardless of min-idle-time until none remain while concurrently processing new entries until shutdown.
+  /// This is useful for facilitating work-resumation for use cases in which there's guaranteed to be at most one consumer and a restart occurs; such case
+  /// requires no idle-delay to claim pending work because it can be reasoned the old consumer is offline and has no pending work in progress.
+  ClaimAllPending,
+  /// Process new entries until shutdown and while concurrently clearing the idle-pending backlog until none remain and max_idle has elapsed
   ClearBacklog {
-    /// min-idle-time filter for XAUTOCLAIM, which transfers ownership to this consumer of messages pending for more than <min-idle-time>
+    /// min-idle-time filter for XAUTOCLAIM, which transfers ownership to this consumer of messages pending for more than <min-idle-time>.
     min_idle: Duration,
     /// max-idle time relative to reactor start time to await newly idle entries before proceeding to only claim new entries
     max_idle: Option<Duration>,
   },
-  /// Process new entries while continuously autoclaiming entries from other consumers that have not been acknowledged within the span of min_idle
+  /// Process new entries until shutdown while continuously autoclaiming entries from other consumers that have not been acknowledged within the span of min_idle
   Autoclaim {
     /// min-idle-time filter for XAUTOCLAIM, which transfers ownership to this consumer of messages pending for more than <min-idle-time>
     min_idle: Duration,
@@ -523,6 +527,31 @@ where
     }
 
     match claim_mode {
+      ClaimMode::ClaimAllPending => {
+        if matches!(self.group_status, ConsumerGroupState::PreviouslyCreated) {
+          try_join!(
+            backoff::future::retry(ExponentialBackoff::default(), || async {
+              self
+                .process_idle_pending(&Duration::new(0, 0), &Some(Duration::new(0, 0)))
+                .await?;
+
+              Ok(())
+            }),
+            backoff::future::retry(ExponentialBackoff::default(), || async {
+              self.process_stream().await?;
+
+              Ok(())
+            })
+          )?;
+        } else {
+          backoff::future::retry(ExponentialBackoff::default(), || async {
+            self.process_stream().await?;
+
+            Ok(())
+          })
+          .await?;
+        }
+      }
       ClaimMode::ClearBacklog { min_idle, max_idle } => {
         if matches!(self.group_status, ConsumerGroupState::PreviouslyCreated) {
           try_join!(
